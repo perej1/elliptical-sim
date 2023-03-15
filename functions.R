8bfd8b5bbe8066c34bd95a5b4c214c91ce60b817
perej1 <jaakko.pere@aalto.fi>
2023-03-15 16:55:45 +0200

    Copy functions to a separate script

--- functions.R
+++ functions.R
@@ -1,6 +1,5 @@
-# Estimate elliptical extreme quantile region for clover distribution with
-# certain parameters. Save the resulting figure.
 library(optparse)
+library(mvtnorm)
 library(ggplot2)
 suppressPackageStartupMessages(library(dplyr))
 
@@ -18,6 +17,13 @@ sqrtmat <- function(sigma) {
   }
   eigenvec <- eigen(sigma)$vectors
   eigenvec %*% diag(eigenval^0.5) %*% t(eigenvec)
+}
+
+
+tdist_extreme_region <- function(sigma, gamma, p, m) {
+  w <- get_ball_mesh(m)
+  lambda <- sqrtmat(d * stats::qf(1 - p, d, 1 / gamma) * sigma)
+  w %*% t(lambda)
 }
 
 
@@ -41,6 +47,29 @@ elliptical_extreme_qregion <- function(data, mu_est, sigma_est, p, k, m) {
   # Estimate extreme quantile region
   lambda <- sqrtmat(r_hat^2 * sigma_est)
   sweep(w %*% t(lambda), 2, mu_est, "+")
+}
+
+
+depth_extreme_qregion <- function(data, p, k, m) {
+  n <- nrow(data)
+  w <- get_ball_mesh(m)
+  
+  radius <- apply(data, 1, norm, type = "2")
+  radius_sort <- sort(radius, decreasing = FALSE)
+  
+  gamma_est <- mean((log(radius_sort[(n - k):n]) - log(radius_sort[n - k]))[-1])
+  u_est <- radius_sort[n - k]
+  nu_hat_unit_halfspace <- 1 / k * apply(w %*% t(data) >= u_est, 1, sum)
+  hd_w_nu_hat_star <- sweep(pmax(w %*% t(w), 0)^(-1 / gamma_est), 2,
+                            nu_hat_unit_halfspace, "*")
+  hd_w_nu_hat_star <- apply(hd_w_nu_hat_star, 1, min, na.rm = TRUE)
+  
+  data_unit <- sweep(data, 1, radius, "/")
+  x_approx_w <- apply(data_unit %*% t(w), 1, which.max)
+  nu_s_hat <- 1 / k * sum(radius / u_est >= hd_w_nu_hat_star[x_approx_w]^gamma_est)
+  
+  r <- u_est * (k * nu_s_hat / (n * p))^gamma_est * hd_w_nu_hat_star^gamma_est
+  r * w
 }
 
 
@@ -72,7 +101,7 @@ gen_clover <- function(n) {
       y <- runif(1, min = -xy_max, max = xy_max)
       z <- runif(1, min = 0, max = z_max)
     }
-      ret[i, ] <- c(x,y)
+    ret[i, ] <- c(x,y)
   }
   ret
 }
@@ -109,71 +138,43 @@ clover_contour_p <- function(p, m) {
 }
 
 
-option_list <- list(
-  make_option("--n", type = "integer", default = 5000,
-              help = "Sample size"),
-  make_option("--k", type = "character", default = "large",
-              help = "Sample size of the tail"),
-  make_option("--seed", type = "integer", default = 204,
-              help = "Set seed for sampling")
-)
-opt_parser <- OptionParser(option_list = option_list)
-opt <- parse_args(opt_parser)
-
-# Global parameters
-m <- 1000
-
-# Set values for k and p
-k <- switch(opt$k,
-            large = 0.2 * opt$n,
-            medium = 0.1 * opt$n,
-            small = 0.05 * opt$n,
-            rlang::abort("Invalid value of k")
-)
-
-p <- c(2 / opt$n, 1 / (2 * opt$n))
-
-# Generate sample
-set.seed(opt$seed)
-sample <- gen_clover(opt$n)
-colnames(sample) <- c("x", "y")
-
-# Estimate scatter and location
-est <- robustbase::covMcd(sample, alpha = 0.5)
-
-# Compute real and estimated quantile regions
-data_list <- as.list(rep(NA, length(p)))
-for (i in 1:length(p)) {
-  estimate <- elliptical_extreme_qregion(sample, est$center, est$cov, p[i], k, m)
-  real <- clover_contour_p(p[i], m)
+compute_error <- function(real, estimate, m1, m2, f, sigma) {
+  l <- solve(sqrtmat(sigma))
+  estimate <- estimate %*% t(l)
+  real <- real %*% t(l)
+  res <- 0
+  coord <- matrix(NA, ncol = 2, nrow = m1 * m2)
   
-  colnames(estimate) <- c("x", "y")
-  colnames(real) <- c("x", "y")
+  r_real <- apply(real, 1, norm, type = "2")
+  r_estimate <- apply(estimate, 1, norm, type = "2")
   
-  data_list[[i]] <- bind_rows(as_tibble(real), as_tibble(estimate)) %>%
-    mutate(group = rep(paste0(c("real", "estimate"), i), each = 1000))
+  ball_real <- sweep(real, 1, r_real, "/")
+  ball_estimate <- sweep(estimate, 1, r_estimate, "/")
+  
+  theta <- rep(NA, m1)
+  for (i in 1:m1) {
+    theta[i] <- acos(ball_real[i, 1])
+    if (ball_real[i, 2] < 0) {
+      theta[i] <- 2 * pi - theta[i]
+    }
+  }
+  
+  ind <- apply(ball_real %*% t(ball_estimate), 1, which.max)
+  r_estimate <- r_estimate[ind]
+  
+  for (i in 1:m1) {
+    r_seq <- seq(min(r_estimate[i], r_real[i]),
+                 max(r_estimate[i], r_real[i]),
+                 length.out = m2)
+    
+    x <- r_seq * cos(theta[i])
+    y <- r_seq * sin(theta[i])
+    coord[((i-1)*m2+1):(i*m2), ] <- cbind(x,y)
+    resi <- 0
+    for (j in 1:m2) {
+      resi <- resi + f(c(x[j], y[j])) * r_seq[j]
+    }
+    res <- res + abs(r_real[i] - r_estimate[i]) * resi
+  }
+  list(res = 2 * pi / (m1 * m2) * res, coord = coord)
 }
-
-# Plotting
-g <- ggplot(as_tibble(sample), aes(x = x, y = y)) +
-  geom_point() +
-  geom_path(data = bind_rows(data_list),
-            aes(x = x, y = y, group = group, linetype = group),
-            show.legend = FALSE) +
-  coord_fixed() +
-  theme(axis.title = element_blank(),
-        panel.background = element_blank(),
-        axis.line = element_line(colour = "black"),
-        legend.background = element_blank(),
-        legend.key = element_blank(),
-        legend.key.size = unit(1, "cm"),
-        legend.text = element_text(size = 15),
-        axis.text = element_text(size = 15)) +
-  scale_linetype_manual(name = NULL,
-                        values = c("real1" = "solid", "estimate1" = "dashed",
-                                   "real2" = "solid", "estimate2" = "dashed"))
-
-# Save figure
-filename <- paste0("n_", opt$n, "_k_", opt$k, ".jpg")
-ggsave(paste0("clover-figures/", filename), plot = g, width = 7, height = 7,
-       dpi = 1000)
