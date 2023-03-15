b94e1df2a469d9f129c9bfde459c5b145242f23b
perej1 <jaakko.pere@aalto.fi>
2023-03-15 17:56:11 +0200

    Add error computation in simulation script

--- simulate.R
+++ simulate.R
@@ -1,80 +1,11 @@
-library(optparse)
-library(mvtnorm)
-suppressPackageStartupMessages(library(dplyr))
-
-
-get_ball_mesh <- function(m) {
-  w <- seq(0, 2 * pi, length.out = m)
-  cbind(cos(w), sin(w))
-}
-
-
-sqrtmat <- function(sigma) {
-  eigenval <- eigen(sigma)$values
-  if (any(eigenval <= 0) || any(sigma != t(sigma))) {
-    rlang::abort("`sigma` must be a symmetric positive definite matrix.")
-  }
-  eigenvec <- eigen(sigma)$vectors
-  eigenvec %*% diag(eigenval^0.5) %*% t(eigenvec)
-}
-
-
-tdist_extreme_region <- function(sigma, gamma, p, m) {
-  w <- get_ball_mesh(m)
-  lambda <- sqrtmat(d * stats::qf(1 - p, d, 1 / gamma) * sigma)
-  w %*% t(lambda)
-}
-
-
-elliptical_extreme_qregion <- function(data, sigma_est, p, k, m) {
-  n <- nrow(data)
-  w <- get_ball_mesh(m)
-  
-  # Approximate generating variate
-  radius <- sqrt(stats::mahalanobis(data, FALSE, sigma_est, inverted = FALSE))
-  radius_sort <- sort(radius, decreasing = FALSE)
-  
-  # Estimate extreme value index
-  gamma_est <- mean((log(radius_sort[(n - k):n]) - log(radius_sort[n - k]))[-1])
-  
-  # Estimate extreme quantile of generating variate
-  r_hat <- radius_sort[n - k] * (k / (n * p))^gamma_est
-  
-  # Estimate extreme quantile region
-  lambda <- sqrtmat(r_hat^2 * sigma_est)
-  w %*% t(lambda)
-}
-
-
-depth_extreme_qregion <- function(data, p, k, m) {
-  n <- nrow(data)
-  w <- get_ball_mesh(m)
-  
-  radius <- apply(data, 1, norm, type = "2")
-  radius_sort <- sort(radius, decreasing = FALSE)
-  
-  gamma_est <- mean((log(radius_sort[(n - k):n]) - log(radius_sort[n - k]))[-1])
-  u_est <- radius_sort[n - k]
-  nu_hat_unit_halfspace <- 1 / k * apply(w %*% t(data) >= u_est, 1, sum)
-  hd_w_nu_hat_star <- sweep(pmax(w %*% t(w), 0)^(-1 / gamma_est), 2,
-                            nu_hat_unit_halfspace, "*")
-  hd_w_nu_hat_star <- apply(hd_w_nu_hat_star, 1, min, na.rm = TRUE)
-  
-  data_unit <- sweep(data, 1, radius, "/")
-  x_approx_w <- apply(data_unit %*% t(w), 1, which.max)
-  nu_s_hat <- 1 / k * sum(radius / u_est >= hd_w_nu_hat_star[x_approx_w]^gamma_est)
-  
-  r <- u_est * (k * nu_s_hat / (n * p))^gamma_est * hd_w_nu_hat_star^gamma_est
-  r * w
-}
-
+source("functions.R")
 
 option_list <- list(
   make_option("--type", type = "character", default = "cauchy",
               help = "Distribution type"),
   make_option("--n", type = "integer", default = 500,
               help = "Sample size"),
-  make_option("--seed", type = "integer", default = 204,
+  make_option("--seed", type = "integer", default = 278,
               help = "Set seed for sampling"),
   make_option("--p", type = "character", default = "low",
               help = "Probability mass outside quantile region"),
@@ -141,7 +72,7 @@ for (i in 1:s) {
     as.matrix
   est <- robustbase::covMcd(data, alpha = 0.5)
   
-  e_i <- elliptical_extreme_qregion(data, est$cov, p, k, m)
+  e_i <- elliptical_extreme_qregion(data, mu, est$cov, p, k, m)
   elliptical_estimates[[i]] <- e_i
   
   d_i <- depth_extreme_qregion(data, p, k, m)
@@ -165,6 +96,31 @@ real <- tdist_extreme_region(sigma, gamma, p, m)
 colnames(real) <- c("x", "y")
 real <- tibble::as_tibble(real)
 
+# Density function
+f <- function (x) {
+  dmvt(x, mu, diag(2), 1 / gamma, log = FALSE)
+}
+
+# Calculate errors
+elliptical_err <- rep(NA, s)
+depth_err <- rep(NA, s)
+cli::cli_progress_bar("Compute errors", total = s)
+for (i in 1:s) {
+  e_est <- elliptical_estimates %>%
+    select(num_range(c("x", "y"), i)) %>%
+    as.matrix
+  
+  d_est <- depth_estimates %>%
+    select(num_range(c("x", "y"), i)) %>%
+    as.matrix
+  
+  elliptical_err[i] <- compute_error(as.matrix(real), e_est, m, 10, f, sigma)$res / p
+  depth_err[i] <- compute_error(as.matrix(real), d_est, m, 10, f, sigma)$res / p
+  cli::cli_progress_update()
+}
+
+errors <- tibble(elliptical = elliptical_err, depth = depth_err)
+
 # Write data
 filename <- paste0("type_", opt$type,
                    "_n_", opt$n,
@@ -180,6 +136,10 @@ readr::write_csv(elliptical_estimates,
 readr::write_csv(depth_estimates,
                  paste0("data/depth-estimates/", filename)
                  )
+
+readr::write_csv(errors,
+                 paste0("data/errors/", filename)
+)
 
 filename <- paste0("type_", opt$type, "_p_", opt$p, ".csv")
 readr::write_csv(real, paste0("data/real-regions/", filename))
