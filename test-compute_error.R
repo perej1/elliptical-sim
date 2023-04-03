31856cfe64330cab7e59ea7d6cfe947c3f930a76
perej1 <jaakko.pere@aalto.fi>
2023-04-03 19:21:12 +0300

    Add more tests

--- test-compute_error.R
+++ test-compute_error.R
@@ -5,32 +5,73 @@ source("functions.R")
 set.seed(278)
 d <- 2
 m1 <- 1000
-m2 <- 10
+m2 <- 100
 mu <- rep(0, 2)
-p <- c(1 / 1000, 1 / 1500)
-err <- p[1] - p[2]
 gamma <- c(0.1, 0.5, 1)
+tol <- 0.1
 
-
-test_that("Error computation works for spherical t-distriributions", {
+test_that("Error computation works for spherical t-distributions, when
+          estimate is far away from real", {
   sigma <- diag(2)
-
+  p <- c(1 / 1000, 1 / 100)
+  err <- abs(p[1] - p[2])
+  
   for (gamma_i in gamma) {
     real <- tdist_extreme_region(sigma, gamma_i, p[1], m1)
     estimate <- tdist_extreme_region(sigma, gamma_i, p[2], m1)
     err_est <- compute_error(real, estimate, m1, m2, gamma_i, sigma)
-    expect_equal(err / p[1], err_est / p[1], tolerance = 0.01)
+    print(paste0("Real: ", err / p[1]))
+    print(paste0("Estimate: ", err_est / p[1]))
+    expect_equal(err / p[1], err_est / p[1], tolerance = tol)
+  }
+})
+
+test_that("Error computation works for elliptical t-distributions, when
+          estimate is far away from real", {
+  sigma <- matrix(c(11, 10.5, 10.5, 11.25), byrow = TRUE, ncol = 2)
+  p <- c(1 / 1000, 1 / 100)
+  err <- abs(p[1] - p[2])
+  
+  for (gamma_i in gamma) {
+    real <- tdist_extreme_region(sigma, gamma_i, p[1], m1)
+    estimate <- tdist_extreme_region(sigma, gamma_i, p[2], m1)
+    err_est <- compute_error(real, estimate, m1, m2, gamma_i, sigma)
+    print(paste0("Real: ", err / p[1]))
+    print(paste0("Estimate: ", err_est / p[1]))
+    expect_equal(err / p[1], err_est / p[1], tolerance = tol)
   }
 })
 
 
-test_that("Error computation works for elliptical t-distriributions", {
-  sigma <- matrix(c(11, 10.5, 10.5, 11.25), byrow = TRUE, ncol = 2)
+test_that("Error computation works for spherical t-distributions when
+          estimate is close to real", {
+  sigma <- diag(2)
+  p <- c(1 / 1000, 1 / 1500)
+  err <- abs(p[1] - p[2])
 
   for (gamma_i in gamma) {
     real <- tdist_extreme_region(sigma, gamma_i, p[1], m1)
     estimate <- tdist_extreme_region(sigma, gamma_i, p[2], m1)
     err_est <- compute_error(real, estimate, m1, m2, gamma_i, sigma)
-    expect_equal(err / p[1], err_est / p[1], tolerance = 0.01)
+    print(paste0("Real: ", err / p[1]))
+    print(paste0("Estimate: ", err_est / p[1]))
+    expect_equal(err / p[1], err_est / p[1], tolerance = tol)
+  }
+})
+
+
+test_that("Error computation works for elliptical t-distributions, when
+          estimate is close to real", {
+  sigma <- matrix(c(11, 10.5, 10.5, 11.25), byrow = TRUE, ncol = 2)
+  p <- c(1 / 1000, 1 / 1500)
+  err <- abs(p[1] - p[2])
+
+  for (gamma_i in gamma) {
+    real <- tdist_extreme_region(sigma, gamma_i, p[1], m1)
+    estimate <- tdist_extreme_region(sigma, gamma_i, p[2], m1)
+    err_est <- compute_error(real, estimate, m1, m2, gamma_i, sigma)
+    print(paste0("Real: ", err / p[1]))
+    print(paste0("Estimate: ", err_est / p[1]))
+    expect_equal(err / p[1], err_est / p[1], tolerance = tol)
   }
 })
