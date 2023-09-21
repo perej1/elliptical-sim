24930ecedbcfd0200fe53bd7da1bb2da946d554a
perej1 <jaakko.pere@aalto.fi>
2023-09-21 15:29:56 +0300

    Remove clover distribution example

--- simulate-batch.R
+++ simulate-batch.R
@@ -5,9 +5,7 @@ option_list <- list(
   make_option("--simulate", type = "logical", default = TRUE,
               help = "Simulate or not"),
   make_option("--summarise", type = "logical", default = TRUE,
-              help = "Summarise or not"),
-  make_option("--clover", type = "logical", default = TRUE,
-              help = "Generate clover example or not")
+              help = "Summarise or not")
 )
 opt_parser <- OptionParser(option_list = option_list)
 opt <- parse_args(opt_parser)
@@ -62,30 +60,4 @@ if (opt$summarise) {
   }
 
   cli::cli_h1("Summary done")
-}
-
-if (opt$clover) {
-  cli::cli_h1("Clover example starting")
-
-  n <- 5000
-  k <- c("small", "medium", "large")
-  seed <- 278
-
-  arg <- expand.grid(n = n, k = k, seed = seed)
-  arg_vector <- sprintf("Rscript estimate-clover.R --n %d --k %s --seed %d",
-                        arg$n,
-                        arg$k,
-                        arg$seed)
-
-  n_scenario <- length(arg_vector)
-  for (i in 1:n_scenario) {
-    cli::cli_h2("Scenario {i}/{n_scenario} running")
-    cli::cli_alert_info("Parameters:
-                      . \t n={arg$n[i]}
-                      . \t k={arg$k[i]}
-                      . \t seed={arg$seed[i]}")
-    system(arg_vector[i])
-  }
-
-  cli::cli_h1("Clover example done")
 }
