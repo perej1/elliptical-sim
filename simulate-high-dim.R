4981dbdbf0bf4cd59a74e4598620b19405889e4b
perej1 <jaakko.pere@aalto.fi>
2024-01-19 13:47:26 +0200

    Remove argument parsing from high dim simulation

--- simulate-high-dim.R
+++ simulate-high-dim.R
@@ -1,7 +1,5 @@
 # Perform simulation for one scenario.
-library(optparse)
 source("functions.R")
-
 
 option_list <- list(
   make_option("--s", type = "integer", default = 10,
@@ -20,48 +18,58 @@ option_list <- list(
 opt_parser <- OptionParser(option_list = option_list)
 opt <- parse_args(opt_parser)
 
-# Global parameters
-gamma <- 1
-sigma <- diag(opt$d)
-mu <- rep(0, opt$d)
-
-# Set values for p and k
-p <- switch(opt$p,
-            low = 2 / opt$n,
-            medium = 1 / opt$n,
-            high = 1 / (2 * opt$n),
-            rlang::abort("Invalid value of p")
-)
-
-k <- switch(opt$k,
-            large = 0.2 * opt$n,
-            medium = 0.1 * opt$n,
-            small = 0.05 * opt$n,
-            rlang::abort("Invalid value of k")
-)
-
-simulate <- function(i) {
-  # Generate sample
-  data <- mvtnorm::rmvt(opt$n, sigma, 1 / gamma, mu)
+simulate <- function(s, d, n, p, k, seed) {
+  # Global parameters
+  gamma <- 1
+  sigma <- diag(d)
+  mu <- rep(0, d)
   
-  # Estimate scatter
-  est <- robustbase::covMcd(data, alpha = 0.5)
+  # Set values for p and k
+  p <- switch(p,
+              low = 2 / n,
+              medium = 1 / n,
+              high = 1 / (2 * n),
+              rlang::abort("Invalid value of p")
+  )
   
-  # Estimate extreme value index and extreme quantile of generating variate
-  radius <- sqrt(stats::mahalanobis(data, FALSE, est$cov, inverted = FALSE))
-  radius_sort <- sort(radius, decreasing = FALSE)
+  k <- switch(opt$k,
+              large = 0.2 * n,
+              medium = 0.1 * n,
+              small = 0.05 * n,
+              rlang::abort("Invalid value of k")
+  )
   
-  gamma_est <- mean((log(radius_sort[(opt$n - k):opt$n])
-                - log(radius_sort[opt$n - k]))[-1])
-  r_hat <- radius_sort[opt$n - k] * (k / (opt$n * p))^gamma_est
+  simulate_round <- function(i) {
+    # Generate sample
+    data <- mvtnorm::rmvt(n, sigma, 1 / gamma, mu)
+    
+    # Estimate scatter
+    est <- robustbase::covMcd(data, alpha = 0.5)
+    
+    # Estimate extreme value index and extreme quantile of generating variate
+    radius <- sqrt(stats::mahalanobis(data, FALSE, est$cov, inverted = FALSE))
+    radius_sort <- sort(radius, decreasing = FALSE)
+    
+    gamma_est <- mean((log(radius_sort[(n - k):n])
+                       - log(radius_sort[n - k]))[-1])
+    r_hat <- radius_sort[n - k] * (k / (n * p))^gamma_est
+    
+    # Compute conservative estimate for the error
+    compute_error_elliptical(est$cov, r_hat, 1 / gamma, p)
+  }
   
-  # Compute conservative estimate for the error
-  compute_error_elliptical(est$cov, r_hat, 1 / gamma, p)
+  # Compute median error from s repetitions and the number of NAs
+  set.seed(seed)
+  error <- purrr::map_dbl(1:s, simulate_round)
+  
+  list(error_med = median(error, na.rm = TRUE), num_na = sum(is.na(error)))
 }
 
-# Compute median error from s repetitions and the number of NAs
-set.seed(opt$seed)
-error <- purrr::map_dbl(1:opt$s, simulate)
+s <- 10
+d <- 2
+n <- 5000
+p <- "high"
+k <- "large"
+seed <- 278
 
-num_na <- sum(is.na(error))
-error_med <- median(error, na.rm = TRUE)
+simulate(s, d, n, p, k, seed)
